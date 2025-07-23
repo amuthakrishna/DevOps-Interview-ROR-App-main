31c0eedfb49175a7755e8f5db536c85e99635dbf
Krishnamoorthy <krishnamoorthy@pinnacleseven.com>
2025-07-23 17:35:54 +0530

    rails-app

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,3 @@
-
 module "vpc" {
   source               = "./modules/vpc"
   vpc_cidr             = var.vpc_cidr
@@ -26,13 +25,29 @@ module "alb" {
 }
 
 module "s3" {
-  source        = "./modules/s3"
-  env_s3_bucket = "my-ecs-env-bucket"
-  env_s3_key    = "app.env"
-  db_host       = module.rds.db_endpoint
-  s3_bucket_name = "my-ecs-env-bucket"
-  aws_region     = var.aws_region
-  lb_endpoint    = module.alb.alb_dns_name
+  source      = "./modules/s3"
+  bucket_name = "my-ecs-env-bucket"
+}
+
+data "template_file" "rails_env" {
+  template = file("${path.module}/modules/s3/env/rails-app.env.tpl")
+
+  vars = {
+    db_name        = var.db_name
+    db_username    = var.db_username
+    db_password    = var.db_password
+    db_host        = module.rds.rds_endpoint
+    lb_endpoint    = module.alb.alb_dns_name
+    s3_bucket_name = module.s3.bucket_name
+    aws_region     = var.aws_region
+  }
+}
+
+resource "aws_s3_object" "rails_env_file" {
+  bucket       = module.s3.bucket_name
+  key          = "env/rails-app.env"
+  content      = data.template_file.rails_env.rendered
+  content_type = "text/plain"
 }
 
 module "ecs" {
@@ -43,31 +58,8 @@ module "ecs" {
   alb_sg_id                 = module.alb.alb_sg_id
   container_image_webserver = var.container_image_webserver
   container_image_nginx     = var.container_image_nginx
-  env_s3_bucket             = module.s3.env_s3_bucket_name
-  env_s3_key                = module.s3.env_s3_key
+  env_s3_bucket             = module.s3.bucket_name
+  env_s3_key                = aws_s3_object.rails_env_file.key
   aws_region                = var.aws_region
   project_name              = var.project_name
-}
-
-
-#module "ecr" {
-#  source        = "./modules/ecr"
-#  ecr_repo_name = "laravel"
-#}
-data "template_file" "rails_env" {
-  template = file("${path.module}/modules/s3/env/rails-app.env.tpl")
-
-  vars = {
-    db_host         = module.rds.rds_endpoint
-    lb_endpoint     = module.alb.alb_dns_name
-    s3_bucket_name  = module.s3.bucket_name
-    aws_region      = var.aws_region
-  }
-}
-
-resource "aws_s3_object" "rails_env" {
-  bucket = module.s3.bucket_name
-  key    = "env/rails-app.env"
-  content = data.template_file.rails_env.rendered
-  content_type = "text/plain"
 }
