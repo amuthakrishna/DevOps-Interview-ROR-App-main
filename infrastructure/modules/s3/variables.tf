3145908533206314693ed3ea7c2bad360d667a9b
Krishnamoorthy <krishnamoorthy@pinnacleseven.com>
2025-07-23 21:37:14 +0530

    rails-app

--- infrastructure/modules/s3/variables.tf
+++ infrastructure/modules/s3/variables.tf
@@ -1,8 +1,3 @@
-variable "bucket_name" {
-  description = "S3 bucket name for environment file"
-  type        = string
-  default = "rail-app"
-}
 
 variable "aws_region" {
   description = "AWS region where S3 bucket is created"
@@ -12,6 +7,7 @@ variable "aws_region" {
 variable "env_s3_bucket" {
   description = "Name of the S3 bucket containing the .env file"
   type        = string
+  default = "rail-app"
 }
 variable "env_s3_key" {
   description = "Key path to the .env file in the S3 bucket"
